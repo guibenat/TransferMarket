03717e06ad4a9af9fa0322fccfa5ac1887e0347e
guibenatte <guibenattedev@gmail.com>
2026-04-07 19:49:14 -0300

    Inicio da lógica main

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,3 +1,30 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    boolean continuarPrograma = true;
+    Scanner sc = new Scanner(System.in);
+
+    do {
+        System.out.println("Bem vindo ao Mercado de transferencais, digite o que voce busca: ");
+        System.out.println("Digite [1] para cadastrar seu jogador.");
+        System.out.println("Digite [2] para listar jogadores.");
+        System.out.println("Digite [3] para atualizar seu jogador.");
+        System.out.println("Digite [4] para deletar seu jogador.");
+        int opcaoUser = sc.nextInt();
+
+        switch (opcaoUser){
+            case 1:
+
+            case 2:
+                break;
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+        }
+
+
+    } while()
 }
